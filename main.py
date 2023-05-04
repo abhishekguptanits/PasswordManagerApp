d212c24920edbdec8e16197ca441ad3c012e18ce
Abhishek <abhishekguptanits@gmail.com>
2023-05-04 12:21:16 +0530

    added search password functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -34,23 +35,89 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    text = f"{website} | {email} | {password}\n"
+    new_dict = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    # if len(website) <= 0 or len(email) <= 0 or len(password) <= 0:
+    #     messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
+    # else:
+    #     with open("data.json", mode="w") as file:
+    #         # # Write json data to file
+    #         # json.dump(obj=new_dict, fp=file, indent=4)
+    #
+    #         # # Read json data from file
+    #         # data = json.load(fp=file)
+    #         # print(data)
+    #         # print(type(data))
+    #
+    #         # Update json data in a file
+    #         data = json.load(fp=file)
+    #         data.update(new_dict)
+    #         json.dump(obj=data, fp=file, indent=4)
 
     if len(website) <= 0 or len(email) <= 0 or len(password) <= 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
     else:
-        # Check with user if they are satisfied with entered details
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"Confirm if below details entered are correct before saving: "
-                                               f"\nEmail: {email} \n Password: {password}")
-        if is_ok:
+        try:
+            with open("data.json", mode="r") as file:
+                # Reading old data
+                data = json.load(fp=file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file:
+                json.dump(obj=new_dict, fp=file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_dict)
+            with open("data.json", mode="w") as file:
+                # Saving updated data
+                json.dump(obj=data, fp=file, indent=4)
+        finally:
+            messagebox.showinfo(title="Credentials Saved", message="Your credentials are saved successfully")
             website_entry.delete(0, END)
             email_entry.delete(0, END)
             email_entry.insert(0, "aryan@gmail.com")
             password_entry.delete(0, END)
-            with open("data.txt", mode="a") as file:
-                file.write(text)
-                messagebox.showinfo(title="Success", message="Information saved successfully")
+
+
+# ---------------------------- Search Password ------------------------------- #
+
+# def search():
+#     website = website_entry.get()
+#     try:
+#         with open("data.json", 'r') as file:
+#             data = json.load(fp=file)
+#             try:
+#                 credentials = data[website]
+#             except KeyError as error_msg:
+#                 messagebox.showerror(title="Record not found", message=f"Key {error_msg} doesn't exist, Try again "
+#                                                                        f"with different input OR add new details")
+#             else:
+#                 messagebox.showinfo(title=f"{website}",
+#                                     message=f"Email: {credentials['email']}\nPassword: {credentials['password']}")
+#     except FileNotFoundError:
+#         messagebox.showerror(title="No data exist", message="No data has been saved yet. "
+#                                                             "Try with adding new data first")
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No data has been saved yet. Try with adding new data first")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+        else:
+            messagebox.showinfo(title=website, message=f"No data found for '{website}', "
+                                                       f"Try again with different input OR add new details")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -74,9 +141,9 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entrys / Inputs
-website_entry = Entry(width=38)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 
 email_entry = Entry(width=38)
 email_entry.insert(0, "aryan@gmail.com")
@@ -86,10 +153,13 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=12, command=find_password)
+search_button.grid(row=1, column=2)
+
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=35, command=save)
+add_button = Button(text="Add", bg="#654E92", width=35, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
